cfe56e2048dfa694b5fb712aa410d24e0579c3ee
Iaroslav Postovalov <postovalovya@gmail.com>
2021-02-28 14:40:18 +0700

    Rework without MST

--- kmath-units/src/commonMain/kotlin/space/kscience/kmath/units/units.kt
+++ kmath-units/src/commonMain/kotlin/space/kscience/kmath/units/units.kt
@@ -1,42 +1,99 @@
 package space.kscience.kmath.units
 
-import space.kscience.kmath.ast.MST
-import space.kscience.kmath.ast.MST.*
 import space.kscience.kmath.operations.*
+import kotlin.jvm.JvmName
+import kotlin.math.PI
 
-public typealias Measure = MST
+/**
+ * Represents base units of International System of Units.
+ */
+public enum class BaseUnits {
+    /**
+     * The base unit of time.
+     */
+    SECOND,
 
-public data class Measurement<T>(val measure: MST, val value: T)
+    /**
+     * The base unit of length.
+     */
+    METER,
+
+    /**
+     * The base unit of mass.
+     */
+    KILOGRAM,
+
+    /**
+     * The base unit of electric current.
+     */
+    AMPERE,
+
+    /**
+     * The base unit of thermodynamic temperature.
+     */
+    KELVIN,
+
+    /**
+     * The base unit of amount of substance.
+     */
+    MOLE,
+
+    /**
+     * The base unit of luminous intensity.
+     */
+    CANDELA;
+}
+
+/**
+ * Represents a unit.
+ *
+ * @property chain chain of multipliers consisting of [BaseUnits] unit and its power.
+ * @property multiplier a scalar to multiply an element applied to this unit.
+ */
+public data class Measure internal constructor(
+    val chain: Map<BaseUnits, Int> = emptyMap(),
+    val multiplier: Double = 1.0,
+)
+
+internal fun Measure(vararg chain: Pair<BaseUnits, Int>, multiplier: Double = 1.0): Measure =
+    Measure(mapOf(*chain), multiplier)
+
+public data class Measurement<T>(val measure: Measure, val value: T)
 
 public object MeasureAlgebra : Algebra<Measure> {
     public const val MULTIPLY_OPERATION: String = "*"
     public const val DIVIDE_OPERATION: String = "/"
 
-    public val pure: Measure = Numeric(1)
-    public val m: Measure = Symbolic("m")
-    public val kg: Measure = Symbolic("kg")
-    public val s: Measure = Symbolic("s")
-    public val A: Measure = Symbolic("A")
-    public val K: Measure = Symbolic("K")
-    public val mol: Measure = Symbolic("mol")
-    public val cd: Measure = Symbolic("cd")
+    public fun multiply(a: Measure, b: Measure): Measure {
+        val newChain = mutableMapOf<BaseUnits, Int>()
 
-    public fun multiply(a: Measure, b: Measure): Measure = Binary(MULTIPLY_OPERATION, a, b)
-    public fun divide(a: Measure, b: Measure): Measure = Binary(DIVIDE_OPERATION, a, b)
+        a.chain.forEach { (k, v) ->
+            val cur = newChain[k]
+            if (cur != null) newChain[k] = cur + v
+            else newChain[k] = v
+        }
+
+        b.chain.forEach { (k, v) ->
+            val cur = newChain[k]
+            if (cur != null) newChain[k] = cur + v
+            else newChain[k] = v
+        }
+
+        return Measure(
+            mapOf(*newChain
+                .entries
+                .sortedBy { (k, _) -> k.ordinal }
+                .map(MutableMap.MutableEntry<BaseUnits, Int>::toPair)
+                .toTypedArray()),
+            a.multiplier * b.multiplier,
+        )
+    }
+
+    public fun divide(a: Measure, b: Measure): Measure =
+        multiply(a, b.copy(chain = b.chain.mapValues { (_, v) -> -v }))
 
     public operator fun Measure.times(b: Measure): Measure = multiply(this, b)
     public operator fun Measure.div(b: Measure): Measure = divide(this, b)
-
-    public override fun bindSymbol(value: String): Measure = when (value) {
-        "m" -> m
-        "kg" -> kg
-        "s" -> s
-        "A" -> A
-        "K" -> K
-        "mol" -> mol
-        "cd" -> cd
-        else -> super.bindSymbol(value)
-    }
 
     override fun binaryOperationFunction(operation: String): (left: Measure, right: Measure) -> Measure =
         when (operation) {
@@ -46,48 +103,131 @@ public object MeasureAlgebra : Algebra<Measure> {
         }
 }
 
-public val rad: Measure get() = MeasureAlgebra.pure
-public val sr: Measure get() = MeasureAlgebra.pure
-public val degC: Measure get() = MeasureAlgebra.K
-public val Hz: Measure get() = MeasureAlgebra { pure / s }
-public val N: Measure get() = MeasureAlgebra { kg * m * (pure / (s * s)) }
-public val J: Measure get() = MeasureAlgebra { N * m }
-public val W: Measure get() = MeasureAlgebra { J / s }
-public val Pa: Measure get() = MeasureAlgebra { N / (m * m) }
-public val lm: Measure get() = MeasureAlgebra { cd * sr }
-public val lx: Measure get() = MeasureAlgebra { lm / (m * m) }
-public val C: Measure get() = MeasureAlgebra { A * s }
-public val Ω: Measure get() = MeasureAlgebra { V / A }
-public val V: Measure get() = MeasureAlgebra { J / C }
-public val F: Measure get() = MeasureAlgebra { C / V }
-public val Wb: Measure get() = MeasureAlgebra { kg * m * m * (pure / (s * s)) * (pure / A) }
-public val T: Measure get() = MeasureAlgebra { Wb / (m * m) }
-public val H: Measure get() = MeasureAlgebra { kg * m * m * (pure / (s * s)) * (pure / (A * A)) }
-public val S: Measure get() = MeasureAlgebra { pure / Ω }
-public val Bq: Measure get() = MeasureAlgebra { pure / s }
-public val Gy: Measure get() = MeasureAlgebra { J / kg }
-public val Sv: Measure get() = MeasureAlgebra { J / kg }
-public val kat: Measure get() = MeasureAlgebra { mol / s }
+/**
+ * A measure for dimensionless quantities with multiplier `1.0`.
+ */
+public val pure: Measure = Measure()
+
+/**
+ * A measure for [BaseUnits.METER] of power `1.0` with multiplier `1.0`.
+ */
+public val m: Measure = Measure(BaseUnits.METER to 1)
+
+/**
+ * A measure for [BaseUnits.KILOGRAM] of power `1.0` with multiplier `1.0`.
+ */
+public val kg: Measure = Measure(BaseUnits.KILOGRAM to 1)
+
+/**
+ * A measure for [BaseUnits.SECOND] of power `1.0` with multiplier `1.0`.
+ */
+@get:JvmName("s-seconds")
+public val s: Measure = Measure(BaseUnits.SECOND to 1)
+
+/**
+ * A measure for [BaseUnits.AMPERE] of power `1.0` with multiplier `1.0`.
+ */
+public val A: Measure = Measure(BaseUnits.AMPERE to 1)
+
+/**
+ * A measure for [BaseUnits.KELVIN] of power `1.0` with multiplier `1.0`.
+ */
+public val K: Measure = Measure(BaseUnits.KELVIN to 1)
+
+/**
+ * A measure for [BaseUnits.MOLE] of power `1.0` with multiplier `1.0`.
+ */
+public val mol: Measure = Measure(BaseUnits.MOLE to 1)
+
+/**
+ * A measure for [BaseUnits.CANDELA] of power `1.0` with multiplier `1.0`.
+ */
+public val cd: Measure = Measure(BaseUnits.CANDELA to 1)
+
+public val rad: Measure = pure
+public val sr: Measure = pure
+public val degC: Measure = K
+public val Hz: Measure = MeasureAlgebra { pure / s }
+public val N: Measure = MeasureAlgebra { kg * m * (pure / (s * s)) }
+public val J: Measure = MeasureAlgebra { N * m }
+public val W: Measure = MeasureAlgebra { J / s }
+public val Pa: Measure = MeasureAlgebra { N / (m * m) }
+public val lm: Measure = MeasureAlgebra { cd * sr }
+public val lx: Measure = MeasureAlgebra { lm / (m * m) }
+public val C: Measure = MeasureAlgebra { A * s }
+public val V: Measure = MeasureAlgebra { J / C }
+public val Ohm: Measure = MeasureAlgebra { V / A }
+public val F: Measure = MeasureAlgebra { C / V }
+public val Wb: Measure = MeasureAlgebra { kg * m * m * (pure / (s * s)) * (pure / A) }
+
+@get:JvmName("T-tesla")
+public val T: Measure = MeasureAlgebra { Wb / (m * m) }
+
+@get:JvmName("H-henry")
+public val H: Measure = MeasureAlgebra { kg * m * m * (pure / (s * s)) * (pure / (A * A)) }
+
+public val S: Measure = MeasureAlgebra { pure / Ohm }
+public val Bq: Measure = MeasureAlgebra { pure / s }
+public val Gy: Measure = MeasureAlgebra { J / kg }
+public val Sv: Measure = MeasureAlgebra { J / kg }
+public val kat: Measure = MeasureAlgebra { mol / s }
+
+public val g: Measure = Measure(BaseUnits.KILOGRAM to 1, multiplier = 0.001)
+public val min: Measure = Measure(BaseUnits.SECOND to 1, multiplier = 60.0)
+public val h: Measure = Measure(BaseUnits.SECOND to 1, multiplier = 3600.0)
+public val d: Measure = Measure(BaseUnits.SECOND to 1, multiplier = 86_400.0)
+public val au: Measure = Measure(BaseUnits.METER to 1, multiplier = 149_597_870_700.0)
+public val deg: Measure = Measure(multiplier = PI / 180.0)
+public val arcMin: Measure = Measure(multiplier = PI / 10_800.0)
+public val arcS: Measure = Measure(multiplier = PI / 648_000.0)
+public val ha: Measure = Measure(BaseUnits.METER to 2, multiplier = 10000.0)
+public val l: Measure = Measure(BaseUnits.METER to 3, multiplier = 0.001)
+public val t: Measure = Measure(BaseUnits.KILOGRAM to 1, multiplier = 1000.0)
+public val Da: Measure = Measure(BaseUnits.KILOGRAM to 1, multiplier = 1.660_539_040_202_020 * 10e-27)
+public val eV: Measure = J.copy(multiplier = 1.602_176_634 * 10e-19)
+
+public fun Y(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e24)
+public fun Z(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e21)
+public fun E(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e18)
+public fun P(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e15)
+public fun T(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e12)
+public fun G(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e9)
+public fun M(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e6)
+public fun k(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e3)
+public fun h(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e2)
+public fun da(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e1)
+
+public fun y(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-24)
+public fun z(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-21)
+public fun a(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-18)
+public fun f(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-15)
+public fun p(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-12)
+public fun n(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-9)
+public fun u(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-6)
+public fun m(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-3)
+public fun c(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-2)
+public fun d(measure: Measure): Measure = measure.copy(multiplier = measure.multiplier * 1e-1)
 
 public open class MeasurementSpace<T>(public open val algebra: Space<T>) : Space<Measurement<T>> {
     public override val zero: Measurement<T>
-        get() = Measurement(MeasureAlgebra.pure, algebra.zero)
+        get() = Measurement(pure, algebra.zero)
 
     public override fun add(a: Measurement<T>, b: Measurement<T>): Measurement<T> {
-        require(a.measure == b.measure) { "The units are incompatible." }
-        return Measurement(a.measure, algebra { a.value + b.value })
+        require(a.measure.chain == b.measure.chain) { "The units are incompatible. The chains are ${a.measure.chain} and ${b.measure.chain}" }
+        return Measurement(Measure(a.measure.chain),
+            algebra { a.value * a.measure.multiplier + b.value * b.measure.multiplier })
     }
 
     public override fun multiply(a: Measurement<T>, k: Number): Measurement<T> =
         Measurement(a.measure, algebra { a.value * k })
 
-    public operator fun Pair<T, Measure>.unaryPlus(): Measurement<T> = Measurement(second, first)
+    public operator fun T.times(m: Measure): Measurement<T> = Measurement(m, this)
 }
 
 public open class MeasurementRing<T>(override val algebra: Ring<T>) : MeasurementSpace<T>(algebra),
     Ring<Measurement<T>> {
     public override val one: Measurement<T>
-        get() = Measurement(MeasureAlgebra.pure, algebra.one)
+        get() = Measurement(pure, algebra.one)
 
     public override fun multiply(a: Measurement<T>, b: Measurement<T>): Measurement<T> =
         Measurement(MeasureAlgebra { a.measure * b.measure }, algebra { a.value * b.value })
@@ -102,7 +242,7 @@ public open class MeasurementField<T>(public override val algebra: Field<T>) : M
 public open class MeasurementExtendedField<T>(public override val algebra: ExtendedField<T>) :
     MeasurementField<T>(algebra),
     ExtendedField<Measurement<T>> {
-    public override fun number(value: Number): Measurement<T> = Measurement(MeasureAlgebra.pure, algebra.number(value))
+    public override fun number(value: Number): Measurement<T> = Measurement(pure, algebra.number(value))
     public override fun sin(arg: Measurement<T>): Measurement<T> = Measurement(arg.measure, algebra.sin(arg.value))
     public override fun cos(arg: Measurement<T>): Measurement<T> = Measurement(arg.measure, algebra.cos(arg.value))
     public override fun tan(arg: Measurement<T>): Measurement<T> = Measurement(arg.measure, algebra.tan(arg.value))
--- kmath-units/src/jvmMain/kotlin/main.kt
+++ kmath-units/src/jvmMain/kotlin/main.kt
@@ -1,12 +1,19 @@
 import space.kscience.kmath.operations.RealField
-import space.kscience.kmath.operations.invoke
-import space.kscience.kmath.units.MeasureAlgebra
-import space.kscience.kmath.units.MeasurementExtendedField
-import space.kscience.kmath.units.N
+import space.kscience.kmath.units.*
 
 public fun main() {
-    val res = with(MeasurementExtendedField(RealField)) {
-        +(1.0 to N) + +(23.0 to MeasureAlgebra { kg * m * (pure / (s * s)) })
+    var res = with(MeasurementExtendedField(RealField)) {
+        val a = (2.0 * kg) / (3.0 * m) / (2.0 * with(MeasureAlgebra) { s * s })
+        val b = (23.0 * Pa)
+        a + b
+    }
+
+    println(res)
+
+    res = with(MeasurementExtendedField(RealField)) {
+        val a = (2.0 * G(m)) + (3.0 * M(m))
+        val b = (3.0 * au)
+        a + b
     }
 
     println(res)
